9a8470e78b5c44eb214d8e7c45f5e63d7b98b7bf
Alena Elhajassaad <alenaelhajassaad@yahoo.com>
2022-10-31 12:11:19 -0400

    AccountRepository methods finished

--- spring_13-Data-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
+++ spring_13-Data-CinemaLab/src/main/java/com/cydeo/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import com.cydeo.entity.Account;
 import com.cydeo.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,13 +15,13 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to list all accounts with a specific country or state
-     List<Account> findByCountryOrState(String country, String state);
+     List<Account> findAllByCountryOrState(String country, String state);
 
     //Write a derived query to list all accounts with age lower than or equal to a specific value
-    List<Account> findByAgeLessThanOrAgeEquals(int age);
+    List<Account> findByAgeLessThanOrAgeEquals(Integer age);
 
     //Write a derived query to list all accounts with a specific role
-    List<Account> findByRole(String role);
+    List<Account> findAllByRole(UserRole role);
 
     //Write a derived query to list all accounts between a range of ages
     List<Account> findByAgeBetween(int age1, int age2);
@@ -42,24 +43,27 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> adminAccounts(UserRole role);
 
     //Write a JPQL query to sort all accounts with age
-    @Query("select e from Account e order by e.age")
+    @Query("select e from Account e order by e.age desc")
     List<Account> sortedByAge();
 
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all accounts with an age lower than a specific value
-    @Query(value = "select * from Account where age <?1", nativeQuery=true)
-    List<Account> ageLowerThan(int age);
+    @Query(value = "select * from account_details where age <?1", nativeQuery=true)
+    List<Account> ageLowerThan(Integer age);
 
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state city
-    @Query(value = "select * from Account where name like '%?1%'", nativeQuery = true)
-    List<Account> ifContains (String value);
-
+    @Query(value = "select * from account_details ad where name ilike concat ('%',?1,'%')"+
+            "or country ilike concat ('%',?1,'%')"+
+            "or address ilike concat ('%',?1,'%')"+
+            "or ad.state ilike concat ('%',?1,'%')"+
+            "or city ilike concat ('%',?1,'%')", nativeQuery = true)
+    List<Account> ifContains (@Param("pattern") String value);
 
     //Write a native query to read all accounts with an age higher than a specific value
-    @Query(value="select * from Account where age > ?1", nativeQuery = true)
-    List<Account> ageHigherThan(int age);
+    @Query(value="select * from account_details where age > ?1", nativeQuery = true)
+    List<Account> ageHigherThan(Integer age);
 
 
 }
