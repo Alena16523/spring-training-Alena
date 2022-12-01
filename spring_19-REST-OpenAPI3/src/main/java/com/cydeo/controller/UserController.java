942fd5756e6777ea8b1de8724435e462e77fda0b
Alena <alenaelhajassaad@yahoo.com>
2022-12-01 15:51:21 -0500

    API creation

---
+++ spring_19-REST-OpenAPI3/src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/list")
+    @Operation(summary = "Read all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved users (OK)",
+                    content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @Operation(summary = "Create an User")
+    @ApiResponse(responseCode = "201", description = "User created successfully (CREATED)",
+               content = {@Content(mediaType = "application/xml"),@Content(mediaType = "application/json")},
+                headers = {@Header(name = "Connection", description = "keep-alive")})
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(userDTO));
+    }
+
+}
