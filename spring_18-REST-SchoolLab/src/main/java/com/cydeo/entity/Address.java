cb2e2e8a0c192521a4bcd48d6c5ee6835cbe49ae
Alena <alenaelhajassaad@yahoo.com>
2022-12-01 10:50:46 -0500

    solved lab

--- spring_18-REST-SchoolLab/src/main/java/com/cydeo/entity/Address.java
+++ spring_18-REST-SchoolLab/src/main/java/com/cydeo/entity/Address.java
@@ -1,6 +1,7 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.AddressType;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
